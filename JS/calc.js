c110c292a411d160548dc1faa704b345420367e0
Pietro Rocchio <pietro.rocchio@gmail.com>
2025-10-26 22:58:38 +0100

    feat(calc): Implement calculator operations and update event listeners

--- JS/calc.js
+++ JS/calc.js
@@ -1,33 +1,70 @@
 let result = 0;
 
-// Array<HTMLButtonElement>
+// NodeListOf<HTMLButtonElement>
 let allBtn = document.querySelectorAll("button");
 
-allBtn.forEach((btn) => {
-    console.log(btn);
-    //btn.innerText = "cioa"
+let val1 = document.getElementById("val1");
+let val2 = document.getElementById("val2");
 
+allBtn.forEach((btn) => {
     btn.addEventListener("click", (event) => {
         console.log(event.target);
     });
+
+    btn.addEventListener("click", operate);
 });
 
+function operate(event){
+    let operator = event.target.id;
+    let a = parseFloat(val1.value);
+    let b = parseFloat(val2.value);
+
+    result = performOperation(operator, a, b);
+
+    if (result instanceof Error) {
+        console.error(result.message);
+        document.getElementById("result").innerText = "Error: " + result.message;
+        return;
+    }
+
+    if (!isNaN(result)) {
+        let r = 0;
+        result = result.toFixed(4);
+    }
+
+    console.log("Result: " + result);
+    document.getElementById("result").innerText = "Result: " + result;
+}
 
 
+function performOperation(operator, a, b){
+    switch(operator){
+        case "btn-op-sum":
+            return sum(a,b);
+        case "btn-op-sub":
+            return sub(a,b);
+        case "btn-op-div":
+            return div(a,b);
+        case "btn-op-mul":
+            return mol(a,b);
+        default:
+            return Error("Operazione non valida");
+    }
+}
 
 
-function somma(a,b){
+function sum(a,b){
     return a + b;
 }
 
-function sottrazione(a,b){
+function sub(a, b){
     return a - b;
 }
 
-function divisione(a,b){
+function div(a,b){
     return a / b;
 }
 
-function moltiplicazione(a,b){
+function mol(a,b){
     return a * b;
 }
