22bf8962c06499142f1d1542909729e5a1f55a85
Pietro Rocchio <pietro.rocchio@gmail.com>
2025-10-26 22:41:43 +0100

    feat(calc): Add basic calculator functions and button event listeners

--- JS/calc.js
+++ JS/calc.js
@@ -1,1 +1,33 @@
 let result = 0;
+
+// Array<HTMLButtonElement>
+let allBtn = document.querySelectorAll("button");
+
+allBtn.forEach((btn) => {
+    console.log(btn);
+    //btn.innerText = "cioa"
+
+    btn.addEventListener("click", (event) => {
+        console.log(event.target);
+    });
+});
+
+
+
+
+
+function somma(a,b){
+    return a + b;
+}
+
+function sottrazione(a,b){
+    return a - b;
+}
+
+function divisione(a,b){
+    return a / b;
+}
+
+function moltiplicazione(a,b){
+    return a * b;
+}
